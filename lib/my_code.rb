3f83d5f68d8375fadcdb8f8d3cdb45b045342d9c
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-17 23:37:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def map_to_square(source_array)
   arr 
 end 
 
-def reduce_to_total(source_array, starting_point = 100)
+def reduce_to_total(source_array, starting_point = nil)
   total = 0 
  
   i = 0 
